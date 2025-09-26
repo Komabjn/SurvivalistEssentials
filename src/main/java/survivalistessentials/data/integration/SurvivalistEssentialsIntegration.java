feaa102624328bafa85e53c9b5ad792361cc4727
Wendall Cada <wendallc@83864.com>
2025-09-26 09:08:31 -0700

    Support auto-configuring more tool types.

--- src/main/java/survivalistessentials/data/integration/SurvivalistEssentialsIntegration.java
+++ src/main/java/survivalistessentials/data/integration/SurvivalistEssentialsIntegration.java
@@ -43,6 +43,8 @@ public final class SurvivalistEssentialsIntegration {
     public static final String CARRYON_MODID = "carryon";
     public static final String REGIONS_MODID = "regions_unexplored";
     public static final String TETRA_MODID = "tetra";
+    public static final String MINECRAFT_MODID = "minecraft";
+
 
     public static Item AQUA_DRIFTWOOD;
     public static Item CHERRY_PLANKS;
@@ -516,6 +518,10 @@ public final class SurvivalistEssentialsIntegration {
 
     public static ResourceLocation tetraLoc(String name) {
         return getLoc(TETRA_MODID, name);
+    }
+
+    public static ResourceLocation mcLoc(String name) {
+        return getLoc(MINECRAFT_MODID, name);
     }
 
     private static ResourceLocation getLoc(String modid, String name) {
