34c7edd85d2765175674e10de28a74d31a00af4a
Wendall Cada <wendallc@83864.com>
2025-10-23 11:39:02 -0700

    Update registration order so Blocks are loaded before BlockItems. Babric is too dumb.

--- Fabric/src/main/java/survivalistessentials/SurvivalistEssentialsFabric.java
+++ Fabric/src/main/java/survivalistessentials/SurvivalistEssentialsFabric.java
@@ -3,13 +3,16 @@ package survivalistessentials;
 import java.util.function.BiConsumer;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
 
 import survivalistessentials.common.HarvestBlock;
 import survivalistessentials.common.loot.SurvivalistEssentialsLootConditionTypes;
@@ -20,17 +23,13 @@ import survivalistessentials.world.SurvivalistEssentialsWorld;
 import survivalistessentials.world.effect.SurvivalistEssentialsEffects;
 import survivalistessentials.world.feature.SurvivalistEssentialsFeatures;
 
-import static survivalistessentials.util.ResourceLocationHelper.prefix;
-
 public class SurvivalistEssentialsFabric implements ModInitializer {
 
 	@Override
     public void onInitialize() {
-        SurvivalistEssentialsFeatures.setup();
-
         registryInit();
-
         SurvivalistEssentials.init();
+        SurvivalistEssentialsFeatures.setup();
 
         /*
          * Not 100% sure if this is the best event to hook into for ensuring all tags are loaded
@@ -46,15 +45,25 @@ public class SurvivalistEssentialsFabric implements ModInitializer {
         });
     }
 
+    /*
+     * Note: The order of these initializations can be important due to dependencies between registries
+     * for example, items that depend on blocks being registered first.
+     */
     private void registryInit() {
         SurvivalistEssentialsFeatures.init(bind(BuiltInRegistries.FEATURE));
         SurvivalistEssentialsEffects.init(bind(BuiltInRegistries.MOB_EFFECT));
         SurvivalistEssentialsIntegration.init(bind(BuiltInRegistries.ITEM));
         SurvivalistEssentialsItems.init(bind(BuiltInRegistries.ITEM));
-        SurvivalistEssentialsWorld.initItems(bind(BuiltInRegistries.ITEM));
         SurvivalistEssentialsWorld.initBlocks(bind(BuiltInRegistries.BLOCK));
+        SurvivalistEssentialsWorld.initItems(bind(BuiltInRegistries.ITEM));
         SurvivalistEssentialsLootConditionTypes.init(bind(BuiltInRegistries.LOOT_CONDITION_TYPE));
         SurvivalistEssentialsSounds.init(bind(BuiltInRegistries.SOUND_EVENT));
+
+        BiomeModifications.addFeature(
+            BiomeSelectors.foundInOverworld(),
+            Decoration.TOP_LAYER_MODIFICATION,
+            SurvivalistEssentialsFeatures.PLACED_LOOSE_ROCKS_KEY
+        );
     }
 
     private static <T> BiConsumer<T, ResourceLocation> bind(Registry<? super T> registry) {
